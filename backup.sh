cdd57b529f9a119cfe90fbb5bcc97ded0a1e13f3
Ali Barış Öztrük <alibarisozturk@gmail.com>
2026-01-22 16:38:28 +0300

    no pass script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker exec mysql8042 mysqldump -uroot -ptv11k52 --databases godojogo --routines --triggers --events --single-transaction --set-gtid-purged=OFF > mysql-init/01-schema.sql
+docker exec mysql8042 mysqldump -uroot --databases godojogo --routines --triggers --events --single-transaction --set-gtid-purged=OFF > mysql-init/01-schema.sql
