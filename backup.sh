fbd656db2c084a4b12de773926cf1998b167d24b
Ali Barış Öztrük <alibarisozturk@gmail.com>
2026-01-27 13:46:18 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -2,4 +2,3 @@
 MYSQL_ROOT_PASSWORD=`docker exec mysql8042 printenv MYSQL_ROOT_PASSWORD`
 cmd="mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --databases godojogo --routines --triggers --events --single-transaction --set-gtid-purged=OFF"
 docker exec mysql8042 $cmd > mysql-init/01-schema.sql
-
